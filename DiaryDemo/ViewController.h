affcc5eff9abdba56890fb1bd412b19dae72ae0e
sherlockqi <xiaqi90@163.com>
2019-04-30 16:50:13 +0800

    Initial Commit

---
+++ DiaryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiaryDemo
+//
+//  Created by PDA-iOS on 2019/4/30.
+//  Copyright © 2019 HeiKki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DiaryDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DiaryDemo
+//
+//  Created by PDA-iOS on 2019/4/30.
+//  Copyright © 2019 HeiKki. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
